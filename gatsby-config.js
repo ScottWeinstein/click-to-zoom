f1e8610f2529d808d4d9650adda5ae95fa476662
Scott Weinstein <scottwww@squidnet.com>
2020-06-03 09:16:37 -0400

    init version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    zoomDomain: 'zoom.us',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+    },
+    'gatsby-plugin-mdx',
+  ],
+};
---
+++ src/components/zoom-link.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+export interface IZoomLinkProps {
+  name: string;
+  mid: string;
+  pwd: string;
+  domain?: string;
+  className?: string;
+}
+
+const ZoomLink = (p: IZoomLinkProps) => {
+  if (p.name == null) {
+    throw new Error('name must not be empty: ' + JSON.stringify(p));
+  }
+  if (p.pwd == null) {
+    throw new Error('pwd must not be empty: ' + JSON.stringify(p));
+  }
+
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          zoomDomain
+        }
+      }
+    }
+  `);
+
+  const id = p.mid.replace(/-/g, '');
+  const pwd = p.pwd.replace(/-/g, '');
+  const meetingPostfix = p.mid.substring(p.mid.length - 5);
+  const domain = p.domain || data.site.siteMetadata.zoomDomain;
+  const iscustom = isNaN(Number(id));
+  const zoomUrl = `zoommtg://${domain}/join?confno=${id}?pwd=${pwd}`;
+  const copyId = (e) => navigator.clipboard.writeText(id);
+  const copyPwd = (e) => navigator.clipboard.writeText(pwd);
+  return (
+    <div className={p.className}>
+      {iscustom ? (
+        <>
+          {p.name}
+          <button onClick={copyId}>cpy id {id}</button>
+          <button onClick={copyPwd}>cpy pwd {pwd}</button>
+        </>
+      ) : (
+        <>
+          <a href={zoomUrl} onClick={copyPwd}>
+            {p.name}
+          </a>
+
+          <small>{meetingPostfix}</small>
+        </>
+      )}
+    </div>
+  );
+};
+export default ZoomLink;
